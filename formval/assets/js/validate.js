3f48cae199c4e056b513bfae9deb0004c7cd43a5
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2021-10-20 08:03:01 +0530

    okie.

--- formval/assets/js/validate.js
+++ formval/assets/js/validate.js
@@ -1,11 +1,10 @@
 function validateForm() {
-  let email_ref = document.getElementsByName("email").value;
+  let email_ref = document.getElementById("email").value;
   //alert(email_ref);
   if (email_ref == "") {
     document.getElementById("message").innerHTML = "Please Enter Email id";
     return false;
   }
-
   let user_password = document.getElementById("password").value;
   if (user_password == "") {
     document.getElementById("passwordMessage").innerHTML =
